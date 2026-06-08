a1ba2abf9a495791711477ba0b4a6999c3e891ee
achmadfachriadi-coder <achmadfachriadi@gmail.com>
2026-06-08 13:59:33 +0700

    Fix URL endpoint Ngrok (Beneran Lolos)

--- pwa-app/app/loading-ai/page.tsx
+++ pwa-app/app/loading-ai/page.tsx
@@ -39,7 +39,7 @@ export default function LoadingAIPage() {
         formData.append("file", blob, "foto-kamar.jpg");
 
         // 1. Tembak ke Python API! 🚀
-        const pythonRes = await fetch("https://agreeing-untangled-pacifier.ngrok-free.dev", {
+        const pythonRes = await fetch("https://agreeing-untangled-pacifier.ngrok-free.dev/analisis-foto", {
           method: "POST",
           body: formData
         });
